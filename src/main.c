3593aae131b404eef0ccedbe17914c541029a6c8
leandra <leahlimalive@gmail.com>
2022-06-12 21:55:24 -0300

    condições para o led com botão e vet interrup

--- src/main.c
+++ src/main.c
@@ -24,49 +24,61 @@ register */
 #define STM32_GPIO_OTYPER_OFFSET 0x0004 /* GPIO port output type register */
 #define STM32_GPIO_PUPDR_OFFSET 0x000c /* GPIO port pull-up/pull-down
 register */
+#define STM32_GPIO_ODR_OFFSET     0x0014  /* GPIO port output data register */
 #define STM32_GPIO_BSRR_OFFSET 0x0018 /* GPIO port bit set/reset register */
 
 /* Register Addresses *******************************************************/
-#define STM32_RCC_AHB1ENR (STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
+#define STM32_RCC_AHB1ENR        (STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
 
-#define STM32_GPIOC_MODER (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
-#define STM32_GPIOC_OTYPER (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
-#define STM32_GPIOC_PUPDR (STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOC_MODER        (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
+#define STM32_GPIOC_OTYPER       (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
+#define STM32_GPIOC_PUPDR        (STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOC_ODR          (STM32_GPIOC_BASE+STM32_GPIO_ODR_OFFSET)
+#define STM32_GPIOC_BSRR         (STM32_GPIOC_BASE+STM32_GPIO_BSRR_OFFSET)
 
 /* AHB1 Peripheral Clock enable register */
 #define RCC_AHB1ENR_GPIOCEN (1 << 2) /* Bit 2: IO port C clock
 enable */
 
 /* GPIO port mode register */
-#define GPIO_MODER_INPUT (0) /* Input */
-#define GPIO_MODER_OUTPUT (1) /* General purpose output mode */
-#define GPIO_MODER_ALT (2) /* Alternate mode */
-#define GPIO_MODER_ANALOG (3) /* Analog mode */
-#define GPIO_MODER13_SHIFT (26)
-#define GPIO_MODER13_MASK (3 << GPIO_MODER13_SHIFT)
+#define GPIO_MODER_INPUT           (0) /* Input */
+#define GPIO_MODER_OUTPUT          (1) /* General purpose output mode */
+#define GPIO_MODER_ALT             (2) /* Alternate mode */
+#define GPIO_MODER_ANALOG          (3) /* Analog mode */
+
+#define GPIO_MODER_SHIFT(n)        (n << 1)
+#define GPIO_MODER_MASK(n)         (3 << GPIO_MODER_SHIFT(n))
 
 /* GPIO port output type register */
-#define GPIO_OTYPER_PP (0) /* 0=Output push-pull */
-#define GPIO_OTYPER_OD (1) /* 1=Output open-drain */
-#define GPIO_OT13_SHIFT (13)
-#define GPIO_OT13_MASK (1 << GPIO_OT13_SHIFT)
+#define GPIO_OTYPER_PP             (0) /* 0=Output push-pull */
+#define GPIO_OTYPER_OD             (1) /* 1=Output open-drain */
+
+#define GPIO_OT_SHIFT(n)           (n)
+#define GPIO_OT_MASK(n)            (1 << GPIO_OT_SHIFT(n))
 
 /* GPIO port pull-up/pull-down register */
-#define GPIO_PUPDR_NONE (0) /* No pull-up, pull-down */
-#define GPIO_PUPDR_PULLUP (1) /* Pull-up */
-#define GPIO_PUPDR_PULLDOWN (2) /* Pull-down */
-#define GPIO_PUPDR13_SHIFT (26)
-#define GPIO_PUPDR13_MASK (3 << GPIO_PUPDR13_SHIFT)
-#define STM32_GPIOC_BSRR (STM32_GPIOC_BASE + STM32_GPIO_BSRR_OFFSET)
+#define GPIO_PUPDR_NONE            (0) /* No pull-up, pull-down */
+#define GPIO_PUPDR_PULLUP          (1) /* Pull-up */
+#define GPIO_PUPDR_PULLDOWN        (2) /* Pull-down */
+
+#define GPIO_PUPDR_SHIFT(n)        (n << 1)
+#define GPIO_PUPDR_MASK(n)         (3 << (n << 1))
 
 /* GPIO port bit set/reset register */
 #define GPIO_BSRR_SET(n) (1 << (n))
 #define GPIO_BSRR_RST(n) (1 << (n + 16))
 
-#define LED_DELAY (0)
 
-//static char fw_version[] = {'V', '1', '.', '0'};
-static uint32_t led_status;
+
+void delay(uint32_t tick)
+{
+  uint32_t i;
+
+  for (i = 0; i < tick; i++)
+    {
+      asm("nop");
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -87,26 +99,36 @@ int main(int argc, char *argv[])
     
     /* Configura PC13 como saida pull-up off e pull-down off */
     reg = *pGPIOC_MODER;
-    reg &= ~(GPIO_MODER13_MASK);
-    reg |= (GPIO_MODER_OUTPUT << GPIO_MODER13_SHIFT);
-    *pGPIOC_MODER = reg;
+    reg &= ~GPIO_MODER_MASK(13);
+    reg |= (GPIO_MODER_OUTPUT << GPIO_MODER_SHIFT(13));
+    *pGPIOC_MODER = reg;  
+
     reg = *pGPIOC_OTYPER;
-    reg &= ~(GPIO_OT13_MASK);
-    reg |= (GPIO_OTYPER_PP << GPIO_OT13_SHIFT);
+    reg &= ~GPIO_OT_MASK(13);
+    reg |= (GPIO_OTYPER_PP << GPIO_OT_SHIFT(13));
     *pGPIOC_OTYPER = reg;
+
     reg = *pGPIOC_PUPDR;
-    reg &= ~(GPIO_PUPDR13_MASK);
-    reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR13_SHIFT);
+    reg &= ~GPIO_PUPDR_MASK(13);
+    reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR_SHIFT(13));
     *pGPIOC_PUPDR = reg;
 
     while(1)
     {
-        *pGPIOC_BSRR = GPIO_BSRR_SET(13);
-        led_status = 0;
-        for(uint32_t i = 0; i < LED_DELAY; i++);
+      //botão apertado
+        uint32_t led_time;
+        if(GPIO_MODER_INPUT << GPIO_MODER_SHIFT(0) == 0)
+            led_time=500;
+        else 
+            led_time=100;
         *pGPIOC_BSRR = GPIO_BSRR_RST(13);
-        led_status = 1;
-        for(uint32_t i = 0; i < LED_DELAY; i++);
+        delay(led_time);
+
+      /* Desliga LED */
+
+        *pGPIOC_BSRR = GPIO_BSRR_SET(13);
+        delay(led_time);
+      
     }
     
     return EXIT_SUCCESS;
